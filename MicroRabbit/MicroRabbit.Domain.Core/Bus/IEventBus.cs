01b1a7f3d04b3597e44b070aa9e734f5965e8e05
Volodymyr Teodorovych <Volodymyr.Teodorovych@thomsonreuters.com>
2020-07-21 14:43:21 +0300

    added interfaces and classes for Domain.Core.

---
+++ MicroRabbit/MicroRabbit.Domain.Core/Bus/IEventBus.cs
@@ -0,0 +1,17 @@
+﻿using MicroRabbit.Domain.Core.Commands;
+using MicroRabbit.Domain.Core.Events;
+using System.Threading.Tasks;
+
+namespace MicroRabbit.Domain.Core.Bus
+{
+    public interface IEventBus
+    {
+        Task SendCommand<TCommand>(TCommand command) where TCommand : Command;
+
+        void Publish<TEvent>(TEvent @event) where TEvent : Event;
+
+        void Subscribe<TEvent, THandler>() 
+            where TEvent : Event 
+            where THandler : IEventHandler<TEvent>;
+    }
+}
---
+++ MicroRabbit/MicroRabbit.Domain.Core/Events/Event.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MicroRabbit.Domain.Core.Events
+{
+    public abstract class Event
+    {
+        public DateTime TimeStamp { get; protected set; }
+
+        protected Event()
+        {
+            this.TimeStamp = DateTime.UtcNow;
+        }
+    }
+}
