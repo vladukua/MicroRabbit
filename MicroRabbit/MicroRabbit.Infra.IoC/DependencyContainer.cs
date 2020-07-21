7d37c03fb7ee851789d517364f6ca94bd74e0b5e
Volodymyr Teodorovych <Volodymyr.Teodorovych@thomsonreuters.com>
2020-07-21 15:35:33 +0300

    initial commit of Banking microservice.

--- MicroRabbit/MicroRabbit.Infra.IoC/DependencyContainer.cs
+++ MicroRabbit/MicroRabbit.Infra.IoC/DependencyContainer.cs
@@ -7,7 +7,7 @@ namespace MicroRabbit.Infra.IoC
     public class DependencyContainer
     {
         public static void RegisterServices(IServiceCollection services)
-        {
+        { 
             // Domain Bus
             services.AddTransient<IEventBus, RabbitMQBus>();
         }
