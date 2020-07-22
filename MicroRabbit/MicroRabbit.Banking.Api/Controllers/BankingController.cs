1f46fdf5cd22fbb35bff9478262673d3f3b4ebc9
Volodymyr Teodorovych <Volodymyr.Teodorovych@thomsonreuters.com>
2020-07-22 14:31:55 +0300

    Partially complited Banking microservice

---
+++ MicroRabbit/MicroRabbit.Banking.Api/Controllers/BankingController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using MicroRabbit.Banking.Application.Interfaces;
+using MicroRabbit.Banking.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace MicroRabbit.Banking.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BankingController : ControllerBase
+    {
+        private readonly IAccountService _accountService;
+        private readonly ILogger<BankingController> _logger;
+
+        public BankingController(ILogger<BankingController> logger, IAccountService accountService)
+        {
+            _logger = logger;
+            _accountService = accountService;
+        }
+
+        [HttpGet]
+        public ActionResult<IReadOnlyCollection<Account>> Get()
+        {
+            return Ok(_accountService.GetAccounts());
+        }
+    }
+}
---
+++ MicroRabbit/MicroRabbit.Banking.Data/Repository/AccountRepository.cs
@@ -0,0 +1,22 @@
+﻿using MicroRabbit.Banking.Data.Context;
+using MicroRabbit.Banking.Domain.Interfaces;
+using MicroRabbit.Banking.Domain.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MicroRabbit.Banking.Data.Repository
+{
+    public class AccountRepository : IAccountRepository
+    {
+        private readonly BankingDbContext _context;
+
+        public AccountRepository(BankingDbContext context)
+        {
+            this._context = context;
+        }
+        public IReadOnlyCollection<Account> GetAccounts()
+        {
+            return _context.Accounts.ToList();
+        }
+    }
+}
